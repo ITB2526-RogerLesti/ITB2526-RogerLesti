f75df718a2555636037aeede09285af197d14a6b
roger.lesti.7e9 <roger.lesti.7e9@itb.cat>
2025-10-01 09:56:22 +0200

    tasca

--- ComprovarEdat.py
+++ ComprovarEdat.py
@@ -1,4 +1,10 @@
 edat=int(input("Quina edat tens?"))
 if edat>=20:
     print("Ets major d'edat")
-    print("Programa Finalitazat")
+    print("Molt bé pots votar")
+    print("Pots conducir")
+else:
+    print("Ets menor d'edat")
+    print("No pots vota encara")
+    print("Vius amb els teus pares")
+print("Programa acabat")
